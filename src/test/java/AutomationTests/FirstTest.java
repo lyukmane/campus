c105d66a43723951ae8abc2e4d8e8b189499bec2
lyukmane <dmytroqatest@gmail.com>
2022-05-06 15:25:17 +0300

    test throws

--- src/test/java/AutomationTests/FirstTest.java
+++ src/test/java/AutomationTests/FirstTest.java
@@ -1,7 +1,12 @@
 package AutomationTests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -27,38 +32,145 @@ public class FirstTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000L));
     }
 
+
     @Test
     public void testSearchResultPresent_seleniumInput() {
         //GIVEN
         driver.get("https://www.google.com/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500L));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(100L));
         WebElement input = driver.findElement(By.xpath(".//input[@name='q']"));
         //WHEN
         input.sendKeys("QA automation");
         input.sendKeys(Keys.ENTER);
         //THEN
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//ul")));
-        driver.close();
-        driver.quit();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//ulc")));
 
     }
 
     @Test
-    public void testSearchResultPresent_JsInput() {
+    public void shouldNavigate() throws MalformedURLException {
         //GIVEN
-        driver.get("https://www.google.com/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500L));
-        WebElement input = driver.findElement(By.xpath(".//input[@name='q']"));
+        String testUrl = "https://www.google.com/";
+        String expectedPageTitle = "google";
         //WHEN
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        // set the text
-        jsExecutor.executeScript("arguments[0].value='QA'", input);
-        input.sendKeys(Keys.ENTER);
+        driver.navigate().to(new URL(testUrl));
+        String actualPageTitle = driver.getTitle();
+        //THEN
+        Assertions.assertEquals(expectedPageTitle, actualPageTitle);
+    }
+
+    @Test
+    public void shouldSearch_byId() {
+        //GIVEN
+        String expectedHeaderText = "Available relative locators";
+        //WHEN
+        driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
+        List<WebElement> headerElements = driver.findElements(By.id("not existing"));
+        //THEN
+        Assertions.assertTrue(headerElements.isEmpty());
+    }
+
+    @Test
+    public void shouldSearch_byName() {
+        //GIVEN
+        //WHEN
+        driver.get("http://online-sh.herokuapp.com/login");
+        WebElement emailInputElement = driver.findElement(By.name("email"));
+        //THEN
+        Assertions.assertTrue(emailInputElement.isEnabled());
+    }
+
+    @Test
+    public void shouldSearch_byClassName() {
+        //GIVEN
+        //WHEN
+        driver.get("http://online-sh.herokuapp.com/login");
+        WebElement submitButtonElement = driver.findElement(By.className("btn-primary"));
+        //THEN
+        Assertions.assertTrue(submitButtonElement.isEnabled());
+    }
+
+    @Test
+    public void shouldSearch_byClassName_multipleElements() {
+        //GIVEN
+        //WHEN
+        driver.get("http://online-sh.herokuapp.com/login");
+        List<WebElement> inputElements = driver.findElements(By.className("form-control"));
 
         //THEN
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//ul")));
+        Assertions.assertEquals(2, inputElements.size());
+
+    }
+
+    @Test
+    public void shouldSearch_byTagName() {
+        //GIVEN
+        int expectedUlsCount = 35;
+        //WHEN
+        driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
+        List<WebElement> headerElements = driver.findElements(By.tagName("ul"));
+        //THEN
+        Assertions.assertEquals(expectedUlsCount, headerElements.size());
+    }
+
+    @Test
+    public void shouldSearch_byLink() {
+        //GIVEN
+        String expectedUrl = "https://www.selenium.dev/documentation/about/contributing/";
+        //WHEN
+        driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
+        WebElement linkElement = driver.findElement(By.partialLinkText("contribution"));
+        linkElement.click();
+        String actualCurrentUrl = driver.getCurrentUrl();
+        //THEN
+        Assertions.assertEquals(expectedUrl, actualCurrentUrl);
+
+    }
+
+    @Test
+    public void shouldSearch_byCssSelector() throws InterruptedException {
+        //GIVEN
+        //WHEN
+        driver.get("http://online-sh.herokuapp.com/login");
+        WebElement inputElement =
+                driver.findElement(By.cssSelector("input[type='Password']"));
+        inputElement.sendKeys("verySecurePassword");
+        //THEN
+        Thread.sleep(1000L);
+
+    }
+
+    @Test
+    public void shouldSearch_byCssSelector_parentChild() {
+        //GIVEN
+        //WHEN
+        driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
+        List<WebElement> elements =
+                driver.findElements(By.cssSelector("ul > li"));
+        //THEN
+        Assertions.assertEquals(177, elements.size());
+
+    }
+
+    @Test
+    public void shouldSearch_byCssSelector_complexStructure() {
+        //GIVEN
+        //WHEN
+        driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
+        List<WebElement> elements =
+                driver.findElements(By.cssSelector("#tabs-10 > li:nth-child(2) > a"));
+        //THEN
+        Assertions.assertEquals(1, elements.size());
+        boolean isElementActive = elements.get(0).getAttribute("class").contains("active");
+        Assertions.assertTrue(isElementActive);
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
         driver.close();
         driver.quit();
-
     }
 }
---
+++ src/test/java/AutomationTests/product.java
@@ -0,0 +1,102 @@
+package AutomationTests;
+
+import calc.my_Calculator;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class product {
+    private static WebDriver driver;
+    public static WebDriverWait wait;
+    @BeforeEach
+    void init() {
+        System.out.println("before each called");
+        System.setProperty("webdriver.chrome.driver", "/Users/dima/Applications/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(100L));
+        }
+
+    @Test
+    public void open_form_user_registration() {
+        //GIVEN
+        String expectedUrl = "http://online-sh.herokuapp.com/registration";
+        driver.get("http://online-sh.herokuapp.com/login");
+        WebElement inputEmail = driver.findElement(By.id("exampleInputEmail1"));
+        //WHEN
+        inputEmail.sendKeys("dmytroessay1@gmail.com");
+        inputEmail.sendKeys(Keys.ENTER);
+        String actualCurrentUrl = driver.getCurrentUrl();
+        //THEN
+        Assertions.assertEquals(expectedUrl, actualCurrentUrl);
+
+    }
+
+    @Test
+    public void new_user_registration() throws InterruptedException {
+        //GIVEN
+        String expectedUrl = "http://online-sh.herokuapp.com/login";
+        driver.get("http://online-sh.herokuapp.com/registration");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(100L));
+        WebElement inputName = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("name")));
+        WebElement inputLastName = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("last_Name")));
+        WebElement inputEmail = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
+        WebElement inputPassword = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
+        WebElement submit = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".btn.btn-primary")));
+
+        //WHEN
+        inputName.sendKeys("Dmytro");
+        inputLastName.sendKeys("Test");
+        inputEmail.sendKeys("dmytroessay1@gmail.com");
+        inputPassword.sendKeys("123456");
+        submit.click();
+        String actualCurrentUrl = driver.getCurrentUrl();
+
+        //THEN
+        Thread.sleep(800l);
+        Assertions.assertEquals(expectedUrl, actualCurrentUrl);
+
+    }
+
+    @Test
+    public void login_existing_user() throws InterruptedException {
+        //GIVEN
+        String expectedUrl = "http://online-sh.herokuapp.com/products";
+        driver.get("http://online-sh.herokuapp.com/login");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(100L));
+
+        WebElement inputEmail = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("exampleInputEmail1")));
+        WebElement inputPassword = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("exampleInputPassword1")));
+        WebElement submit = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".btn.btn-primary")));
+        //WHEN
+        inputEmail.sendKeys("dmytroessay1@gmail.com");
+        inputPassword.sendKeys("123456");
+        submit.click();
+
+        String actualCurrentUrl = driver.getCurrentUrl();
+        //THEN
+        Thread.sleep(800l);
+        Assertions.assertEquals(expectedUrl, actualCurrentUrl);
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        driver.close();
+        driver.quit();
+    }
+    }
+
